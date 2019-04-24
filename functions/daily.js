165377a2987987cbbc4cb160db156e6640191923
Glenn Baker <glenn.baker@envato.com>
2019-04-24 16:11:41 +1000

    Create a function to return daily

--- functions/daily.js
+++ functions/daily.js
@@ -1,6 +1,58 @@
+function random(seed) {
+  var x = Math.sin(seed) * 10000
+  return x - Math.floor(x)
+}
+
+function daysSinceEpoch() {
+  const secondsInADay = 1000 * 60 * 60 * 24
+  const now = new Date()
+  return Math.floor(
+    (now.valueOf() - minutesToMilliseconds(now.getTimezoneOffset())) / secondsInADay
+  )
+}
+
+function minutesToMilliseconds(minutes) {
+  return minutes * 1000 * 60
+}
+
+const potentialChickens = [
+  "lindsay",
+  "riley",
+  "rabidcarrot",
+  "gstamp",
+  "lucas",
+  "khayman",
+  "asellitt",
+  "kelle",
+  "damien.adermann",
+  "claire.grooby",
+  "prasanna",
+  "stacey",
+  "bakes",
+  "riana.ferreira",
+  "mario",
+  "oliver.diestel",
+  "daniel.budden",
+  "bordo",
+  "ben.vilnis",
+  "grant.tibbey",
+  "stevehodgkiss",
+  "Ali Graham",
+  "amanda",
+  "tanya.fonina"
+]
+
 exports.handler = function(event, context, callback) {
+  var seed = daysSinceEpoch()
+
+  const chickens = potentialChickens
+    .map(x => [random(seed++), x])
+    .sort(([a, _a], [b, _b]) => (a > b ? 1 : -1))
+    .map(([_, x]) => x)
+    .slice(0, 5)
+
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: JSON.stringify({ chickens }, null, 2)
   })
 }
